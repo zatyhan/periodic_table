06126bc163449147013256c314b7ed8f3b0b8a02
Nur Eizzaty Hamzaid <nureizzaty@gmail.com>
2024-03-26 23:47:11 +0000

    fix: type id

--- element.sh
+++ element.sh
@@ -11,7 +11,7 @@ else
     atomic_number=$element
     name=$($PSQL "SELECT name FROM elements WHERE atomic_number=$atomic_number")
     symbol=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$atomic_number")
-    type=$($PSQL "SELECT type FROM properties WHERE atomic_number=$atomic_number")
+    type=$($PSQL "SELECT type FROM types WHERE type_id=(SELECT type_id FROM properties WHERE atomic_number=$atomic_number)")
     mass=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$atomic_number")
     melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$atomic_number")
     boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$atomic_number")
@@ -20,7 +20,7 @@ else
     symbol=$element
     name=$($PSQL "SELECT name FROM elements WHERE symbol='$symbol'")
     atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$symbol'")
-    type=$($PSQL "SELECT type FROM properties WHERE atomic_number=$atomic_number")
+    type=$($PSQL "SELECT type FROM types WHERE type_id=(SELECT type_id FROM properties WHERE atomic_number=$atomic_number)")
     mass=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$atomic_number")
     melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$atomic_number")
     boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$atomic_number")
@@ -29,10 +29,16 @@ else
     name=$element
     symbol=$($PSQL "SELECT symbol FROM elements WHERE name='$name'")
     atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE name='$name'")
-    type=$($PSQL "SELECT type FROM properties WHERE atomic_number=$atomic_number")
+    type=$($PSQL "SELECT type FROM types WHERE type_id=(SELECT type_id FROM properties WHERE atomic_number=$atomic_number)")
     mass=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$atomic_number")
     melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$atomic_number")
     boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$atomic_number")
   fi
-  echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+
+  if [[ -z $name ]]
+  then 
+    echo I could not find that element in the database. 
+  else
+    echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+  fi
 fi 
