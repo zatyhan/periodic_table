31134ee48e5e1aee7b9c485c472f2e408233cf5a
Nur Eizzaty Hamzaid <nureizzaty@gmail.com>
2024-03-26 23:39:15 +0000

    refactor: combine all element description features
    feat: element description using atomic_number
    
    feat: element description using symbol
    
    feat: element description using element name

--- element.sh
+++ element.sh
@@ -23,8 +23,6 @@ else
     mass=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$atomic_number")
     melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$atomic_number")
     boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$atomic_number")
-    # echo $name
-
   elif [[ $element =~ ^[A-Z][a-zA-Z]+$ ]]
   then 
     name=$element
@@ -34,9 +32,6 @@ else
     mass=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$atomic_number")
     melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$atomic_number")
     boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$atomic_number")
-
-  else 
-  echo invalid input
   fi
   echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
 fi 
